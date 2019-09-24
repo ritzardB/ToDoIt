52adc646c7bc43cd21522d96896a3bfbb241ec70
Richard Balabarcon <rbalabarcon@outlook.com>
2019-09-24 14:50:06 +0400

    Initial Commit

---
+++ ToDoIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoIt
+//
+//  Created by Richard Balabarcon on 9/24/19.
+//  Copyright © 2019 Richard Balabarcon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
